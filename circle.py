520479a915dec882d9c74ef43e3a6c652e4d9ee2
daimontel <sashs.06@mail.ru>
2026-09-11 15:00:34 +0400

    redact cod structur

--- circle.py
+++ circle.py
@@ -1,8 +1,8 @@
 import math
 class Circle:
-def __init__(self, radius: float):
-if radius <= 0:
-raise ValueError("Радиус должен быть положительным")
-self.radius = radius
+  def __init__(self, radius: float):
+    if radius <= 0:
+      raise ValueError("Радиус должен быть положительным")
+    self.radius = radius
 def area(self) -> float:
-return math.pi * (self.radius ** 2)
+  return math.pi * (self.radius ** 2)
